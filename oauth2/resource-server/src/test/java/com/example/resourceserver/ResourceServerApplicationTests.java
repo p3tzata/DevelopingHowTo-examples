41cc0ed33994b6c682125d818cae9e4f18403fba
Petar Chavdarov <petar.chavdarov@rewe-digital.com>
2023-03-07 11:07:53 +0200

    ss

---
+++ oauth2/resource-server/src/test/java/com/example/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
