c7a4431d696c3154a9fe24da3e7039b8473f7d1b
petar.chavdarov <petar.chavdarov@rewe-digital.com>
2025-11-20 16:13:21 +0200

    save 20 nov

--- howToDev-microservice-monitoring/microservice/src/main/java/com/example/microservice/job/MyJob.java
+++ howToDev-microservice-monitoring/microservice/src/main/java/com/example/microservice/job/MyJob.java
@@ -13,13 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyJob {
 
-  private final MeterProvider<Counter> myCounterProducer;
+  private final Counter myCounterProducer;
   private double gaugeValue = 0.0;
 
   AtomicInteger atomicInteger;
 
   String podName;
   private AtomicInteger gauge;
+
   @Autowired
   public MyJob(MeterRegistry collectorRegistry) {
 
@@ -27,23 +28,28 @@ public class MyJob {
 
     atomicInteger = new AtomicInteger(1);
     myCounterProducer = Counter
-        .builder("my_job_counter_produced_11")
+        .builder("my_job_counter_produced_12")
         .tag("pod", podName)
         .description("myJob Counter Produced")
-        .withRegistry(collectorRegistry);
+        .register(collectorRegistry);
 
-    gauge = collectorRegistry.gauge("my_job_gouge_produced_12",
-        new AtomicInteger(Long.valueOf(getRandomValue()).intValue()));
+    gauge = collectorRegistry
+        .gauge("my_job_gouge_produced_single_4",
+            new AtomicInteger(Long.valueOf(getSingleValue()).intValue()));
 
   }
 
   @Scheduled(fixedRate = 3600000L)
   public void myScheduleCounter() {
 
-    myCounterProducer
-        .withTags(Tags.of("pod", podName, "/Source", "myJob", "/MessageType", "myMessageType", "/MessageStatus",
-            MyJobStatus.SUCCESSFUL.name()))
-        .increment();
+//    myCounterProducer
+//        .withTags(Tags.of("pod", podName, "/Source", "myJob", "/MessageType", "myMessageType", "/MessageStatus",
+//            MyJobStatus.SUCCESSFUL.name()))
+//        .increment();
+
+    myCounterProducer.increment();
+    System.out.println("myScheduleCounter is called");
+
   }
 
   public int getRandomValue() {
@@ -53,10 +59,18 @@ public class MyJob {
     return result;
   }
 
+  public int getSingleValue() {
+
+    System.out.println("getSingleValue is called");
+    return 8;
+  }
+
+  //@Scheduled(cron = "* 10 11 * * ?")
   @Scheduled(fixedRate = 30000L)
   public void myScheduleGauge() {
 
-    gauge.set(getRandomValue());
+
+    gauge.set(getSingleValue());
 
   }
 
